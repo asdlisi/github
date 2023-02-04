1f9a9dc1225ab4946d3507c75bd59a70c2ba8384
laoli <asdasd@qq.com>
2023-02-04 17:58:41 +0800

    github 上传测试2

--- src/main/java/cn/bdqn/test.java
+++ src/main/java/cn/bdqn/test.java
@@ -5,5 +5,6 @@ public class test {
     public static void main(String[] args) {
         System.out.println("github 上传测试");
         System.out.println("github 拉去测试");
+        System.out.println("github 上传测试");
     }
 }
